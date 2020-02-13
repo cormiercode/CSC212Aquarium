1f8795100622451a13b18d320ed038a855ade0f9
kaiacormier <kaiacormier@131.229.138.189>
2020-02-12 21:56:58 -0500

    Now I have bubbles! Bubble task finished, did coloring of screen, started to produce kelp!

--- src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
@@ -2,7 +2,10 @@ package edu.smith.cs.csc212.aquarium;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+//import java.util.ArrayList;
 import me.jjfoley.gfx.GFX;
+import java.util.Random;
+
 
 //Citing: TA Hours 2/9/20
 /**
@@ -18,66 +21,119 @@ import me.jjfoley.gfx.GFX;
  *
  */
 public class Aquarium extends GFX {
-	/**
-	 * This is a static variable that tells us how wide the aquarium is.
-	 */
+	
+	// This is a static variable that tells us how wide the aquarium is.
 	public static int WIDTH = 500;
-	/**
-	 * This is a static variable that tells us how tall the aquarium is.
-	 */
+	
+	//This is a static variable that tells us how tall the aquarium is.
 	public static int HEIGHT = 500;
-
-	/**
-	 * Put a snail on the top of the tank.
-	 */
+	
+	// Put a snail on the top of the tank.
 	Snail algorithm = new Snail(177, Snail.HEIGHT + 1, "top");
+	
+	//initializing random variable for later use
+	Random random = new Random();
+	
+	//initializing bubble array
+	BubbleMachine[] bubbles = new BubbleMachine[10];
 
+	//Set up color change
+	int R = 0;
+	int G = 0;
+	int B = 255;
+	
+	// the rates at which the screen will turn green
+	int dG = 1;
+	int dB = -1;
+	
 	/**
 	 * This is a constructor, code that runs when we make a new Aquarium.
 	 */
+
 	public Aquarium() {
 		// Here we ask GFX to make our window of size WIDTH and HEIGHT.
 		// Don't change this here, edit the variables instead.
 		super(WIDTH, HEIGHT);
-	}
 		
-	
-	//testing out fish class
+		//for loop to create 10 bubbles 
+		for(int i = 0; i < 10; i++) {
+			int bubblex = random.nextInt(50) + 400;
+			int bubbley = random.nextInt(500);
+			int bubblewidth = random.nextInt(5)+ 10;
+			
+			//adding bubbles to array
+			bubbles[i] = new BubbleMachine(Color.CYAN, bubblex, bubbley, bubblewidth, bubblewidth);
+		}
+	}
+
+	// testing out fish class
 	Fish nemo = new Fish(Color.GREEN, 250, 250, true, false);
 	Fish marlin = new Fish(Color.orange, 100, 100, false, false);
 	Fish JJ = new Fish(Color.LIGHT_GRAY, 150, 150, false, true);
-	//Trying to draw a bubble
-	//Cite JJ for bubble code 
-	BubbleMachine one = new BubbleMachine(Color.CYAN, 40, 80, 25, 25);
+	 
 
-	
+	@Override
+	//Our method for drawing all of our objects in the window
 	public void draw(Graphics2D g) {
 		// Draw the "ocean" background.
-		g.setColor(Color.blue);
+		//Color changing code from TA hours 
+		if(G + dG > 255) {
+			dG = -1;
+		}
+		if(G + dG < 0) {
+			dG = 1;
+		}
+		if(B + dB > 255) {
+			dB = -1;
+		}
+		if(B + dB < 0) {
+			dB = 1;
+		}
+		G += dG;
+		B += dB;
+		Color background = new Color(R,G,B);	
+		g.setColor(background);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		
-		//Drawing the fish
+		//draw fish
 		nemo.draw(g);
 		marlin.draw(g);
 		JJ.draw(g);
-		
-		one.draw(g);
-		
-			
-    	// Draw our snail!
-		algorithm.draw(g);
 
+		//draw bubbles
+		for(int i = 0; i < 10; i++) {
+			bubbles[i].draw(g);
+		}
+		
+		// Draw our snail!
+		algorithm.draw(g);
+		
+		//draw treasure chest
+		g.setColor(Color.YELLOW);
+		g.fillRect(380, 465, 115, 50);
+		
+		g.setColor(Color.black);
+		g.fillRect(405, 475, 60, 40);
+
+		g.setColor(Color.yellow);
+		g.fillRect(415, 480, 40, 20);
+		
+		//create seaweed
+		g.setColor(Color.green.darker());
+		g.drawString("~", 40, 30);
+		
+		
 	}
 
 	public static void main(String[] args) {
 		// Uncomment this to make it go slower!
 		// GFX.FPS = 10;
-		// This is potentially helpful for debugging movement if there are too many print statements!
+		// This is potentially helpful for debugging movement if there are too many
+		// print statements!
 
 		// Note that we can store an Aquarium in a variable of type GFX because Aquarium
 		// is a very specific GFX, much like 7 can be stored in a variable of type int!
 		GFX app = new Aquarium();
 		app.start();
 	}
-	}
-	
+}
--- src/main/java/edu/smith/cs/csc212/aquarium/BubbleMachine.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/BubbleMachine.java
@@ -1,34 +1,88 @@
 package edu.smith.cs.csc212.aquarium;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
+//import java.util.Random;
 
 public class BubbleMachine {
 	//Every bubble has a color, x and y starting points, 
 	// a width and a height
 	Color color;
-	int xs;
-	int ys;
+	int x;
+	int y;
+	int wiggle;
 	int w;
 	int h;
+//	int destX; 
+//	int destY;
+//	int speedX; 
+//	int speedY;
+//	int width;
+//	int height;
+	
+	
+	//Top and bottom of the screen for swim function
+	int TOP = 500;
+	int BOTTOM = 0;
 	
 public BubbleMachine(Color c, int startX, int startY, 
 		int width, int height) {
 	this.color = c;
-	this.xs = startX;
-	this.ys = startY;
+	// Making random locations for bubbles 
+	this.x = startX;
+	this.y = startY;
+	this.wiggle = 0;
 	this.w = width; 
 	this.h = height; 
+//	this.destX = 100;
+//	this.destY = 100;
+//	this.speedX = 4;
+//	this.speedY = 2;
+	
+	
 }
 
+//Draw method for BubbleMachine
 public void draw(Graphics2D g) {
-	Shape bubble = new Ellipse2D.Double(this.xs, this.ys, this.w, this.h);
+	//Again, not sure why swim is here?
+//	Shape bubble = new Ellipse2D.Double(this.x + this.wiggle, this.y, this.w, this.h);
+//	
 	
+//	Font f = g.getFont();
+//	g.setFont(f.deriveFont(50.0f));
 	g.setColor(Color.CYAN);
-	g.fill(bubble);
-	g.draw(bubble);
+//	g.drawString("o", this.x + this.wiggle, this.y);
+	g.fillOval(this.x + this.wiggle, this.y, this.w, this.h);
+	
+	animate();
+	}
+
+private void animate() {
+	this.y -= 2;
+	if( this.y < -100) {
+		this.y = 550;
+	}
+	
+	//(int) tells Java we don't care about the fraction
+	this.wiggle = (int) (30.0 * Math.sin(this.y/60.0));
+	// TODO Auto-generated method stub
+	
+}
+
+//swim method for BubbleMachine
+//public void swim( ) {
+//	if( this.y > 0 ) {
+//		this.y -= 1;
+//}
+//	if( this.y < 0 ) {
+//		this.y = 500;
+//	}
+//}
+
+
+
+
 }
 	
 //new addition,may need to comment out
@@ -53,4 +107,3 @@ public void draw(Graphics2D g) {
 		rand.nextInt(HIGH - LOW) + LOW; this.destY = rand.nextInt(HIGH - LOW) + LOW;
 		}
 }*/
-}
